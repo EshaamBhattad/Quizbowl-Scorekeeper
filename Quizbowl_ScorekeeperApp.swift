2cf485270a050f01ae11c83eb1af843b09f3aa76
Eshaam Bhattad <75045105+EshaamBhattad@users.noreply.github.com>
2021-12-24 15:32:02 -0600

    Add files via upload

---
+++ Quizbowl_ScorekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quizbowl_ScorekeeperApp.swift
+//  Quizbowl Scorekeeper
+//
+//  Created by Eshaam Bhattad on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Quizbowl_ScorekeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
